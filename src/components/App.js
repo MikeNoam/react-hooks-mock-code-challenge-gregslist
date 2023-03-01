b5ceba48d46c1fb64c88318bac96696c3049f0d3
Michael Harris <mike.n.harris1@gmail.com>
2023-02-28 21:19:00 -0500

    making progress

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
+
 function App() {
+
+const [listings, setListings] = useState([])
+console.log(listings)
+useEffect( () => {
+  fetch("http://localhost:6001/listings")
+  .then(response => response.json())
+  .then(listingData => setListings(listingData)  )
+}, []
+)
+
   return (
     <div className="app">
       <Header />
-      <ListingsContainer />
+      <ListingsContainer listings = {listings} />
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+//steps:
+//1. fetch data from db.json. 
+//2. employ the useState hook to store the data as a variable.
+//3. employ .map method to loop through the array and generate an item for each object.
+
+
+//
+//
